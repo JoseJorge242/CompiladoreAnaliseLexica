d95c650d6aeead6a5ceab7b06abc5e1188d04a48
JonyPimentel <jonathanpimentel967@gmail.com>
2026-06-13 18:59:24 -0300

    Refatoração

--- src/AnalisadorSintatico/No.java
+++ src/AnalisadorSintatico/No.java
@@ -4,115 +4,94 @@ import java.util.List;
  
 public abstract class No {
  
-    private int linha;
-    private int coluna;
+private int linha;
+private int coluna;
  
-    public No(int linha, int coluna) {
+    protected No(int coluna, int linha) {
         this.linha = linha;
         this.coluna = coluna;
     }
  
-    public int getLinha() {
-        return linha;
-    }
- 
-    public int getColuna() {
-        return coluna;
-    }
+    public int getLinha(){ 
+        return linha;  }
+    public int getColuna(){ 
+        return coluna; }
  
     public static class Programa extends No {
         private List<No> instrucoes;
- 
-        public Programa(List<No> instrucoes, int linha, int coluna) {
-            super(linha, coluna);
-            this.instrucoes = instrucoes;
+        public Programa(List<No> instrucoes) {
+            super(0,  0);
+            this.instrucoes= instrucoes;
         }
- 
-        public List<No> getInstrucoes() {
-            return instrucoes;
-        }
+        public List<No> getInstrucoes(){ 
+            return instrucoes; }
     }
  
     public static class Bloco extends No {
         private List<No> instrucoes;
- 
-        public Bloco(List<No> instrucoes, int linha, int coluna) {
+        public Bloco(int linha,int coluna,List<No> instrucoes) {
             super(linha, coluna);
             this.instrucoes = instrucoes;
         }
- 
-        public List<No> getInstrucoes() {
-            return instrucoes;
-        }
+        public List<No> getInstrucoes(){ 
+            return instrucoes; }
     }
  
     public static class DeclaracaoVariavel extends No {
-        private String nome;
         private String tipo;
+        private String nome;
         private No inicializador;
  
-        public DeclaracaoVariavel(String nome, String tipo, No inicializador, int linha, int coluna) {
-            super(linha, coluna);
-            this.nome = nome;
+        public DeclaracaoVariavel( int coluna, int linha, String tipo, String nome, No inicializador) {
+            super(linha,coluna);
             this.tipo = tipo;
+            this.nome = nome;
             this.inicializador = inicializador;
         }
  
-        public String getNome() {
-            return nome;
-        }
- 
         public String getTipo() {
-            return tipo;
-        }
- 
-        public No getInicializador() {
-            return inicializador;
-        }
+            return tipo;}
+        public String getNome(){ 
+            return nome;}
+        public No getInicializador() { 
+            return inicializador;}
     }
  
     public static class Atribuicao extends No {
         private String nome;
         private String operador;
-        private No valor;
+        private  No valor;
  
-        public Atribuicao(String nome, String operador, No valor, int linha, int coluna) {
+        public Atribuicao(No valor, int coluna, int linha , String nome , String operador) {
             super(linha, coluna);
+             this.valor = valor;
             this.nome = nome;
             this.operador = operador;
-            this.valor = valor;
+           
         }
  
-        public String getNome() {
-            return nome;
-        }
- 
+        public String getNome() { 
+            return nome;}
         public String getOperador() {
-            return operador;
-        }
- 
-        public No getValor() {
-            return valor;
-        }
+             return operador; }
+        public No getValor(){ 
+            return valor;}
     }
  
     public static class IncrDecr extends No {
         private String nome;
         private String operador;
  
-        public IncrDecr(String nome, String operador, int linha, int coluna) {
+        public IncrDecr( String nome,int coluna, String operador, int linha) {
             super(linha, coluna);
-            this.nome = nome;
-            this.operador = operador;
+          this.operador = operador;
+             this.nome = nome;
         }
  
-        public String getNome() {
-            return nome;
-        }
- 
-        public String getOperador() {
-            return operador;
-        }
+        public String getNome(){ 
+            return nome;}
+        public String getOperador() { 
+            return operador;}
     }
  
     public static class Se extends No {
@@ -120,87 +99,70 @@ public abstract class No {
         private Bloco entao;
         private Bloco senao;
  
-        public Se(No condicao, Bloco entao, Bloco senao, int linha, int coluna) {
+        public Se(int linha,No condicao, Bloco entao, Bloco senao, int coluna) {
             super(linha, coluna);
             this.condicao = condicao;
             this.entao = entao;
             this.senao = senao;
         }
  
-        public No getCondicao() {
-            return condicao;
-        }
- 
-        public Bloco getEntao() {
-            return entao;
-        }
- 
-        public Bloco getSenao() {
-            return senao;
-        }
+        public No getCondicao() { 
+            return condicao;}
+        public Bloco getEntao() { 
+            return entao;}
+        public Bloco getSenao() { 
+            return senao;}
     }
- 
     public static class Enquanto extends No {
         private No condicao;
         private Bloco corpo;
  
-        public Enquanto(No condicao, Bloco corpo, int linha, int coluna) {
+        public Enquanto(int linha, No condicao, Bloco corpo, int coluna) {
             super(linha, coluna);
             this.condicao = condicao;
             this.corpo = corpo;
         }
  
-        public No getCondicao() {
-            return condicao;
-        }
- 
-        public Bloco getCorpo() {
-            return corpo;
-        }
+        public No getCondicao() { 
+            return condicao;}
+        public Bloco getCorpo() { 
+            return corpo;}
     }
  
     public static class Para extends No {
-        private No inicializacao;
+        private No init;
         private No condicao;
         private No incremento;
         private Bloco corpo;
  
-        public Para(No inicializacao, No condicao, No incremento, Bloco corpo, int linha, int coluna) {
+        public Para(int linha, int coluna, No condicao, No incremento,No init, Bloco corpo) {
             super(linha, coluna);
-            this.inicializacao = inicializacao;
+            this.init = init;
             this.condicao = condicao;
             this.incremento = incremento;
             this.corpo = corpo;
         }
  
-        public No getInicializacao() {
-            return inicializacao;
-        }
- 
-        public No getCondicao() {
-            return condicao;
-        }
- 
-        public No getIncremento() {
-            return incremento;
-        }
- 
-        public Bloco getCorpo() {
-            return corpo;
-        }
+        public No getInit() { 
+            return init;}
+        public No getCondicao() { 
+            return condicao;}
+        public No getIncremento() { 
+            return incremento;}
+        public Bloco getCorpo(){ 
+            return corpo;}
     }
  
     public static class Retorno extends No {
         private No valor;
  
-        public Retorno(No valor, int linha, int coluna) {
+        public Retorno( int coluna, int linha, No valor) {
             super(linha, coluna);
             this.valor = valor;
         }
  
-        public No getValor() {
-            return valor;
-        }
+        public No getValor() { 
+            return valor;}
     }
  
     public static class Binario extends No {
@@ -208,7 +170,7 @@ public abstract class No {
         private No esquerda;
         private No direita;
  
-        public Binario(String operador, No esquerda, No direita, int linha, int coluna) {
+        public Binario( int coluna,int linha, String operador, No esquerda, No direita) {
             super(linha, coluna);
             this.operador = operador;
             this.esquerda = esquerda;
@@ -216,66 +178,54 @@ public abstract class No {
         }
  
         public String getOperador() {
-            return operador;
-        }
- 
-        public No getEsquerda() {
-            return esquerda;
-        }
- 
-        public No getDireita() {
-            return direita;
-        }
+            return operador;}
+        public No getEsquerda() { 
+            return esquerda;}
+        public No getDireita() { 
+            return direita;}
     }
  
     public static class Unario extends No {
         private String operador;
         private No operando;
  
-        public Unario(String operador, No operando, int linha, int coluna) {
+        public Unario(int linha, int coluna, String operador, No operando) {
             super(linha, coluna);
             this.operador = operador;
             this.operando = operando;
         }
  
         public String getOperador() {
-            return operador;
-        }
- 
+             return operador; }
         public No getOperando() {
-            return operando;
-        }
+             return operando; }
     }
  
     public static class Identificador extends No {
-        private String nome;
+        private final String nome;
  
-        public Identificador(String nome, int linha, int coluna) {
+        public Identificador(int linha, int coluna, String nome) {
             super(linha, coluna);
             this.nome = nome;
         }
  
         public String getNome() {
-            return nome;
-        }
+             return nome; }
     }
  
     public static class Literal extends No {
         private String tipo;
         private String valor;
  
-        public Literal(String tipo, String valor, int linha, int coluna) {
+        public Literal( int coluna, String tipo, String valor, int linha) {
             super(linha, coluna);
             this.tipo = tipo;
             this.valor = valor;
         }
  
-        public String getTipo() {
-            return tipo;
-        }
- 
-        public String getValor() {
-            return valor;
-        }
+        public String getTipo(){
+             return tipo;}
+        public String getValor(){
+             return valor; }
     }
 }
